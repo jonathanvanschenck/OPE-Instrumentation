b4e9f576713d565021071e98e4df0f8b64afa877
jonathanvanschenck <vanschej@oregonstate.edu>
2020-03-21 11:54:40 -0700

    temp backup

---
+++ ope_instrumentation/instruments/.ipynb_checkpoints/resources-checkpoint.py
@@ -0,0 +1,24 @@
+import serial
+import visa
+
+
+class Serial(serial.Serial):
+    TERM = b''
+    BUFFER_SIZE = 1000
+    
+    def __init__(self,*args,**kwargs):
+        serial.Serial.__init__(self,*args,**kwargs)
+        
+        
+    def query(self,msg):
+        """Send query and away response
+        Paramters
+        ---------
+        msg : str
+        """
+        self.write(msg.encode() + self.TERM)
+        rmsg = self.read(self.BUFFER_SIZE)
+        return rmsg.strip(self.TERM).decode()
+    
+    def shutdown(self):
+        self.close()
